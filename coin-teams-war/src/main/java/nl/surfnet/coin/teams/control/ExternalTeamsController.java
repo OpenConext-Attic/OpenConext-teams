2bc78a13966aa4c7fb2217a83b9706fd9ccaf985
Jasha Joachimsthal <jasha.joachimsthal@flex.surfnet.nl>
2012-03-21 16:39:16 +0100

    BACKLOG-368 Retrieve external group names

--- coin-teams-war/src/main/java/nl/surfnet/coin/teams/control/ExternalTeamsController.java
+++ coin-teams-war/src/main/java/nl/surfnet/coin/teams/control/ExternalTeamsController.java
@@ -16,34 +16,96 @@
 
 package nl.surfnet.coin.teams.control;
 
+import java.io.InputStream;
+import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.opensocial.models.Person;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Token;
+import org.scribe.oauth.OAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import nl.surfnet.coin.api.client.OpenConextJsonParser;
+import nl.surfnet.coin.api.client.domain.Group20;
+import nl.surfnet.coin.api.client.domain.Group20Entry;
 import nl.surfnet.coin.teams.domain.GroupProvider;
+import nl.surfnet.coin.teams.domain.GroupProviderUserOauth;
+import nl.surfnet.coin.teams.domain.ThreeLeggedOauth10aGroupProviderApi;
 import nl.surfnet.coin.teams.interceptor.LoginInterceptor;
 import nl.surfnet.coin.teams.service.GroupProviderService;
+import nl.surfnet.coin.teams.service.impl.GroupProviderThreeLeggedOAuth10aService;
+import nl.surfnet.coin.teams.util.GroupProviderOptionParameters;
 
 /**
  * Controller for external teams
  */
 @Controller
+@RequestMapping("/externalgroups/*")
 public class ExternalTeamsController {
 
   @Autowired
   private GroupProviderService groupProviderService;
 
-  @RequestMapping("/externalgroups/myproviders.shtml")
-  public @ResponseBody List<GroupProvider> getMyExternalGroupProviders(HttpServletRequest request) {
+  private OpenConextJsonParser parser = new OpenConextJsonParser();
+
+  @RequestMapping("/myproviders.shtml")
+  public
+  @ResponseBody
+  List<GroupProvider> getMyExternalGroupProviders(HttpServletRequest request) {
     Person person = (Person) request.getSession().getAttribute(
-            LoginInterceptor.PERSON_SESSION_KEY);
+        LoginInterceptor.PERSON_SESSION_KEY);
     return groupProviderService.getGroupProviders(person.getId());
+  }
+
+  @RequestMapping("/mygroups.shtml")
+  public
+  @ResponseBody
+  List<Group20> getMyExternalGroups(HttpServletRequest request) {
+    Person person = (Person) request.getSession().getAttribute(
+        LoginInterceptor.PERSON_SESSION_KEY);
+
+    List<Group20> group20s = new ArrayList<Group20>();
+
+    final List<GroupProviderUserOauth> oauthList = groupProviderService.getGroupProviderUserOauths(person.getId());
+    for (GroupProviderUserOauth oauth : oauthList) {
+      final GroupProvider provider = groupProviderService.getGroupProviderByStringIdentifier(oauth.getProvider());
+
+      ThreeLeggedOauth10aGroupProviderApi api = new ThreeLeggedOauth10aGroupProviderApi(provider);
+      GroupProviderThreeLeggedOAuth10aService tls = new GroupProviderThreeLeggedOAuth10aService(provider, api);
+      final OAuthService oAuthService = tls.getOAuthService();
+
+      Token accessToken = new Token(oauth.getoAuthToken(), oauth.getoAuthSecret());
+      final String strippedID = personToExternalId(person);
+      OAuthRequest oAuthRequest = new OAuthRequest(api.getRequestTokenVerb(),
+          MessageFormat.format("{0}/groups/{1}",
+              provider.getAllowedOptionAsString(GroupProviderOptionParameters.URL),
+              strippedID));
+      oAuthService.signRequest(accessToken, oAuthRequest);
+      Response oAuthResponse = oAuthRequest.send();
+      if (oAuthResponse.isSuccessful()) {
+        InputStream in = oAuthResponse.getStream();
+        final Group20Entry group20Entry = parser.parseGroups20(in);
+        group20s.addAll(group20Entry.getEntry());
+      } else {
+        System.out.println(oAuthResponse.getCode());
+        System.out.println(oAuthResponse.getBody());
+      }
+    }
+    return group20s;
+  }
+
+  private String personToExternalId(Person person) {
+    String internalId = person.getId();
+    String pattern = "urn:collab:(group|person):(.+):(.+)";
+    return internalId.replaceAll(pattern, "$3");
   }
 
 }
