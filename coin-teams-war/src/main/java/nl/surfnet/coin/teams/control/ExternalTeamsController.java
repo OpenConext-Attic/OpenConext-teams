88e4d6590b9a98b7812f61c455f9ce6760a2601c
Jasha Joachimsthal <jasha.joachimsthal@flex.surfnet.nl>
2012-03-26 16:25:45 +0200

    BACKLOG-368 Moved code for actual 3-legged OAuth to a @Service. Converting all group names, id's etc using the filters in the DB

--- coin-teams-war/src/main/java/nl/surfnet/coin/teams/control/ExternalTeamsController.java
+++ coin-teams-war/src/main/java/nl/surfnet/coin/teams/control/ExternalTeamsController.java
@@ -16,40 +16,23 @@
 
 package nl.surfnet.coin.teams.control;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.JsonProcessingException;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.opensocial.models.Person;
-import org.scribe.model.OAuthRequest;
-import org.scribe.model.Response;
-import org.scribe.model.Token;
-import org.scribe.oauth.OAuthService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import nl.surfnet.coin.api.client.OpenConextJsonParser;
 import nl.surfnet.coin.api.client.domain.Group20;
 import nl.surfnet.coin.teams.domain.GroupProvider;
 import nl.surfnet.coin.teams.domain.GroupProviderUserOauth;
-import nl.surfnet.coin.teams.domain.ThreeLeggedOauth10aGroupProviderApi;
 import nl.surfnet.coin.teams.interceptor.LoginInterceptor;
 import nl.surfnet.coin.teams.service.GroupProviderService;
-import nl.surfnet.coin.teams.service.impl.GroupProviderThreeLeggedOAuth10aService;
-import nl.surfnet.coin.teams.util.GroupProviderIdConverter;
-import nl.surfnet.coin.teams.util.GroupProviderOptionParameters;
+import nl.surfnet.coin.teams.service.GroupService;
 
 /**
  * Controller for external teams
@@ -57,14 +40,12 @@ import nl.surfnet.coin.teams.util.GroupProviderOptionParameters;
 @Controller
 @RequestMapping("/externalgroups/*")
 public class ExternalTeamsController {
-  private static Logger log = LoggerFactory.getLogger(ExternalTeamsController.class);
 
   @Autowired
   private GroupProviderService groupProviderService;
 
-  private ObjectMapper objectMapper = new ObjectMapper();
-
-  private OpenConextJsonParser parser = new OpenConextJsonParser();
+  @Autowired
+  private GroupService groupService;
 
   @RequestMapping("/myproviders.shtml")
   public
@@ -85,64 +66,14 @@ public class ExternalTeamsController {
     List<Group20> group20s = new ArrayList<Group20>();
 
     // get a list of my group providers that I already have an access token for
-    final List<GroupProviderUserOauth> oauthList = groupProviderService.getGroupProviderUserOauths(person.getId());
+    final List<GroupProviderUserOauth> oauthList =
+        groupProviderService.getGroupProviderUserOauths(person.getId());
     for (GroupProviderUserOauth oauth : oauthList) {
-
-      // get the group provider
-      final GroupProvider provider = groupProviderService.getGroupProviderByStringIdentifier(oauth.getProvider());
-
-      // we assume now that it's a 3-legged oauth provider
-      ThreeLeggedOauth10aGroupProviderApi api = new ThreeLeggedOauth10aGroupProviderApi(provider);
-      GroupProviderThreeLeggedOAuth10aService tls = new GroupProviderThreeLeggedOAuth10aService(provider, api);
-      final OAuthService oAuthService = tls.getOAuthService();
-
-      Token accessToken = new Token(oauth.getoAuthToken(), oauth.getoAuthSecret());
-      String strippedID = GroupProviderIdConverter.convertToExternalPersonId(provider, person.getId());
-      OAuthRequest oAuthRequest = new OAuthRequest(api.getRequestTokenVerb(),
-          MessageFormat.format("{0}/groups/{1}",
-              provider.getAllowedOptionAsString(GroupProviderOptionParameters.URL),
-              strippedID));
-      oAuthService.signRequest(accessToken, oAuthRequest);
-      Response oAuthResponse = oAuthRequest.send();
-
-      if (oAuthResponse.isSuccessful()) {
-        group20s.addAll(getGroup20sFromResponse(oAuthResponse));
-      } else {
-        log.info("Fetching external groups for user {} failed with status code {}",
-            person.getId(), oAuthResponse.getCode());
-        log.trace(oAuthResponse.getBody());
-      }
-
+      group20s.addAll(groupService.getGroup20s(oauth));
+      // if 1 provider fails, error is returned, but this is not the final UI anyway
     }
     return group20s;
   }
 
-  private List<Group20> getGroup20sFromResponse(Response oAuthResponse) {
-    List<Group20> groups = new ArrayList<Group20>();
-    String body = oAuthResponse.getBody();
-    InputStream in = new ByteArrayInputStream(body.getBytes());
-
-    try {
-      final JsonNode jsonNodes = objectMapper.readTree(body);
-      if (jsonNodes.has("result")) {
-        groups = parser.parseGroup20ResultWrapper(in).getResult().getEntry();
-      } else if (jsonNodes.has("entry")) {
-        groups = parser.parseGroups20(in).getEntry();
-      }
-    } catch (RuntimeException e) {
-      log.warn("Could not parse oAuthResponse into a List of Group20's", e);
-    } catch (JsonProcessingException e) {
-      log.warn("Could not parse oAuthResponse as Json", e);
-    } catch (IOException e) {
-      log.warn("Could not parse oAuthResponse as Json", e);
-    } finally {
-      try {
-        in.close();
-      } catch (IOException e) {
-        log.warn("Could not close input stream from OAuth response body", e);
-      }
-    }
-    return groups;
-  }
 
 }
